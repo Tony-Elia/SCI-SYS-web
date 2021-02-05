fd759b0e26ad59d76cb9750a6226312b3bee3341
unknown <antony.safaga@gmail.com>
2021-02-06 01:13:54 +0200

    w-header

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-var system=navigator.userAgent.toLowerCase();
-if(system.includes("android")){
-  document.getElementById('side').style.display="initial";
-}
-var windows=navigator.userAgent.toLowerCase();
-if(windows.includes("windows")){
-  document.getElementById('site').style.display="initial";
-}
-  
-  function opennav(){
-document.getElementById('mysidenav').style.width="250px";
-document.getElementById('main').style.width="250px";
-  }
-  function closenav(){
-document.getElementById('mysidenav').style.width="0px";
-document.getElementById('main').style.width="0px";
-  }
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelector(".nav-links-li");
+
+hamburger.addEventListener("onclick", () =>{
+  console.log("u clicked me!!");
+  navlinks.classList.toggle("open");
+});
+
+function toggleClass() {
+  console.log("hello there");
+  navlinks.classList.toggle("open");
+};
