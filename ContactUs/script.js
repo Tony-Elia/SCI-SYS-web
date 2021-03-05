efc2532393939db4afd7a91ccde678c745655e1f
unknown <antony.safaga@gmail.com>
2021-03-05 04:25:18 +0200

    contact-us page

---
+++ ContactUs/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    window.setTimeout(function () {
+        document.querySelector('#freepik_stories-contact-us').classList.add('animated');
+        console.log("animated");
+    }, 1000);
+})
--- common.js
+++ common.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navlinks = document.querySelector(".sm-nav-links");
 const navbar = document.querySelector(".nav-bar");
-const goUp = document.querySelector(".go-up");
 
 // toggling the navbar on mobile screens
 hamburger.addEventListener("click", () =>{
@@ -12,17 +11,11 @@ hamburger.addEventListener("click", () =>{
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     navbar.style.backgroundColor = "var(--dark-blue)";
     navbar.style.boxShadow = "0 0 30px var(--dark-blue)";
   } else {
     navbar.style.backgroundColor = "transparent";
     navbar.style.boxShadow = "none";
-  }
-  // toggling the up button
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    goUp.style.opacity = "1";
-  } else {
-    goUp.style.opacity = "0";
   }
 }
