9ee354e7c4080f4523454a77a49b1c798708bf6c
Tony-Elia <antony.safaga@gmail.com>
2021-03-14 01:15:56 +0200

    lazy-load animations

--- common.js
+++ common.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navlinks = document.querySelector(".sm-nav-links");
 const navbar = document.querySelector(".nav-bar");
+const goUp = document.querySelector(".go-up");
+const lazyTargets = document.querySelectorAll(".lazy-load");
 
 // toggling the navbar on mobile screens
 hamburger.addEventListener("click", () =>{
@@ -18,4 +20,33 @@ function scrollFunction() {
     navbar.style.backgroundColor = "transparent";
     navbar.style.boxShadow = "none";
   }
-}
+  // toggling the up button
+  if (goUp) {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+      goUp.style.opacity = "1";
+    } else {
+      goUp.style.opacity = "0";
+    }
+  }
+};
+
+// Lazy load animation
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("show");
+      appearOnScroll.unobserve(entry.target);
+    };
+  })
+}, appearOptions);
+
+lazyTargets.forEach(lazyTarget => {
+  appearOnScroll.observe(lazyTarget);
+});
